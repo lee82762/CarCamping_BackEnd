e895430c00b8cd41063ba96ed9d82003c4a029fb
junbeom-kang <kgood10369@gmail.com>
2021-08-25 04:59:54 +0900

    lazy 방식을 fetch join 을 통하여 Eager 방식으로 변경

--- src/main/java/com/Hanium/CarCamping/repository/ReviewRepository.java
+++ src/main/java/com/Hanium/CarCamping/repository/ReviewRepository.java
@@ -11,20 +11,20 @@ import java.util.List;
 public interface ReviewRepository extends JpaRepository<Review,Long> {
 
 
-    @Query("select r from Review r where r.campSite.campsite_id=:campsite_id order by r.date DESC")
+    @Query("select r from Review r join fetch r.writer m where r.campSite.campsite_id=:campsite_id order by r.date DESC")
     List<Review> findByCampSiteDateDESC(@Param("campsite_id")Long campSite_id);
 
-    @Query("select r from Review r where r.campSite.campsite_id=:campsite_id order by r.date ASC")
+    @Query("select r from Review r join fetch r.writer m where r.campSite.campsite_id=:campsite_id order by r.date ASC")
     List<Review> findByCampSiteDateASC(@Param("campsite_id")Long campSite_id);
 
 
-    @Query("select r from Review r where r.campSite.campsite_id=:campsite_id order by r.score DESC")
+    @Query("select r from Review r join fetch r.writer m where r.campSite.campsite_id=:campsite_id order by r.score DESC")
     List<Review> findByCampSiteDESC(@Param("campsite_id")Long campSite_id);
 
-    @Query("select r from Review r where r.campSite.campsite_id=:campsite_id order by r.score ASC")
+    @Query("select r from Review r join fetch r.writer m where r.campSite.campsite_id=:campsite_id order by r.score ASC")
     List<Review> findByCampSiteASC(@Param("campsite_id")Long campSite_id);
 
-    @Query("select r from Review r where r.campSite.campsite_id=:campsite_id")
+    @Query("select r from Review r join fetch r.writer m where r.campSite.campsite_id=:campsite_id")
     List<Review> findReviewByCampSite(@Param("campsite_id")Long campSite_id);
 
     List<Review> findTop3ByCampSiteOrderByRecommendDesc(CampSite campSite);
