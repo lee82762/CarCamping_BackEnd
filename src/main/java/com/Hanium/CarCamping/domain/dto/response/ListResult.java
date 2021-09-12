d4386354425ed68a9a0d4b41b07b8d2fa265876e
junbeom-kang <kgood10369@gmail.com>
2021-09-13 02:26:28 +0900

    어노테이션 수정

--- src/main/java/com/Hanium/CarCamping/domain/dto/response/ListResult.java
+++ src/main/java/com/Hanium/CarCamping/domain/dto/response/ListResult.java
@@ -1,11 +1,15 @@
 package com.Hanium.CarCamping.domain.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.util.List;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @ToString
 public class ListResult<T> extends Result {
     private List<T> data;
--- src/main/java/com/Hanium/CarCamping/domain/dto/review/ResponseOneReviewDto.java
+++ src/main/java/com/Hanium/CarCamping/domain/dto/review/ResponseOneReviewDto.java
@@ -1,6 +1,7 @@
 package com.Hanium.CarCamping.domain.dto.review;
 
 import com.Hanium.CarCamping.domain.entity.Review;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,7 @@ import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class ResponseOneReviewDto {
     private Long review_id;
     private String writer;
