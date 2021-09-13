619dc8c22aa9a5baa4e729e32b2c03c84901b824
junbeom-kang <kgood10369@gmail.com>
2021-09-14 01:29:27 +0900

    다양한 검색 및 정렬기능 추가

--- src/main/java/com/Hanium/CarCamping/repository/CampSiteRepository.java
+++ src/main/java/com/Hanium/CarCamping/repository/CampSiteRepository.java
@@ -5,6 +5,7 @@ import com.Hanium.CarCamping.domain.entity.CampSite;
 import com.Hanium.CarCamping.domain.entity.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -23,6 +24,10 @@ public interface CampSiteRepository extends JpaRepository<CampSite,Long> {
     List<CampSite> findAllByOrderByScoreDesc();
     List<CampSite> findByRegionOrderByScoreDesc(Region region);
     List<CampSite> findByRegionOrderByScoreAsc(Region region);
+    @Query("select c from CampSite c where c.region = :location order by c.campsite_id ASC")
+    List<CampSite> findByRegionOrderByCampsite_idAsc(@Param("location") Region location);
 
+    @Query("select c from CampSite c where c.region = :location order by c.campsite_id DESC")
+    List<CampSite> findByRegionOrderByCampsite_idDesc(@Param("location") Region location);
     List<CampSite> findByRegistrant(Member member);
 }
