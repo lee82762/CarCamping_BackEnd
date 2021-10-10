75e1b539e9258ab7cb43f729e0fc3350be2bec5c
junbeom-kang <kgood10369@gmail.com>
2021-10-10 18:00:42 +0900

    테스트 코드 제거

--- src/test/java/com/Hanium/CarCamping/service/Member/createAdminTest.java
+++ src/test/java/com/Hanium/CarCamping/service/Member/createAdminTest.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
-
+/*
 @SpringBootTest
 @Transactional
 public class createAdminTest {
@@ -34,3 +34,5 @@ public class createAdminTest {
 
     }
 }
+
+ */
